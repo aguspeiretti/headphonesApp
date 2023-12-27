76528fca93f91dac52c6094eebcc6f7a257d1c6c
agupeiretti <agupeiretti@gmail.com>
2023-12-27 18:30:17 -0300

    productDetail

--- src/screens/ProductDetail.js
+++ src/screens/ProductDetail.js
@@ -1,7 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import productsData from "../data/productsData";
 
-const ProductDetail = () => {
+const ProductDetail = ({ route }) => {
+  const { productId } = route.params;
+  console.log(productId);
+
+  const productSelected = productsData.find(
+    (product) => product.id == productId
+  );
+
+  console.log(productSelected);
+
   return (
     <View>
       <Text>ProductDetail</Text>
